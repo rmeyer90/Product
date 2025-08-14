9c39a7430e5148fd06cefeb966c2f6ed60de7ca7
rmeyer90 <rob.meyer90@gmail.com>
2025-08-14 14:18:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
-
+import ProductCard from "/components/ProductCard";
 export default function App() {
+  const products = [
+    {
+      name: "Kaffeemaschine",
+      price: 89.99,
+      description: "Eine moderne Kaffeemaschine mit Timer.",
+      image:
+        "https://www.bader.de/celum/celum_assets/2019FE9_0068020FCD050_699890_jpg_1_local_l_rd_local_l_rd.jpg/wmf-kaffeemaschine-lumero-fuer-besten-kaffeegenuss-farbe-edelstahl.jpg",
+    },
+    {
+      name: "Bluetooth-Lautsprecher",
+      price: 45.00,
+      description: "Kompakter Lautsprecher mit gutem Klang.",
+      image:
+        "https://www.calibereurope.com/wp-content/uploads/2023/02/093e2589c0a8bbad00dc1eadd7b263f5450e1b14_HPG440BT_Schuin_Top.jpg",
+    },
+    {
+      name: "USB-C Ladegerät",
+      price: 19.99,
+      description: "Schnelles Ladegerät für alle Geräte.",
+      image: "https://nevox.de/media/image/8e/f0/c5/100W.jpg",
+    },
+  ];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
     </div>
   );
 }
